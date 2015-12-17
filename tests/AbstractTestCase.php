c563a5b8db0c8cfa26ac3e3f6072e18fc63b6407
Mauri de Souza Nunes <mauri870@gmail.com>
2015-12-17 14:27:59 -0200

    Create model and test model

---
+++ tests/AbstractTestCase.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author    Mauri de Souza Nunes <mauri870@gmail.com>
+ * @copyright Copyright (c) 2015, Mauri de Souza Nunes <github.com/mauri870>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace Mauri870\Test\Tests;
+
+
+use Orchestra\Testbench\TestCase;
+
+abstract class AbstractTestCase extends TestCase
+{
+    public function migrate(){
+        $this->artisan('migrate',[
+           '--realpath' => realpath(__DIR__."/../src/TestPackage/resource/migrations")
+        ]);
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+}
---
+++ tests/TestPackage/Models/ModelTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author    Mauri de Souza Nunes <mauri870@gmail.com>
+ * @copyright Copyright (c) 2015, Mauri de Souza Nunes <github.com/mauri870>
+ * @license   https://opensource.org/licenses/MIT MIT License
+ */
+
+namespace Mauri870\Test\Tests\Models;
+
+
+use Mauri870\Test\Models\Test;
+use Mauri870\Test\Tests\AbstractTestCase;
+
+class ModelTest extends AbstractTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+        $this->migrate();
+    }
+
+    public function test_check_if_a_data_can_be_persisted(){
+        //$model= Test::create(['name'=>'Teste','description'=>'Descrição']);
+        //$this->assertEquals('Teste',$model->name);
+    }
+}
